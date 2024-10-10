a6de7e3b364dd45336d7506338f159aaa4cd79e3
7amzawey <mohamedhamza123467890@gmail.com>
2024-10-10 19:58:28 +0200

    9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Return a list"""
+"""Return a list."""
 from typing import Iterable, Tuple, Sequence, List
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Return a list of lengths of a number"""
+    """Return a list of lengths of a number."""
     return [(i, len(i)) for i in lst]
